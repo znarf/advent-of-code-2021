ab00dd4aceda3d1ef77c5f24e90f90e57deed450
François Hodierne <francois@hodierne.net>
2021-12-03 21:26:46 +0100

    day 1 part b

--- day-1.js
+++ day-1.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const inputFilename = './day-1.txt';
 const testIputFilename = './day-1-test.txt';
 
-const input = fs.readFileSync(inputFilename, 'utf8').trim().split('\n').map(Number);
+const prepareInput = (filename) => {
+  return fs.readFileSync(filename, 'utf8').trim().split('\n').map(Number);
+};
 
-const testInput = fs.readFileSync(testIputFilename, 'utf8').trim().split('\n').map(Number);
-
-const countNumberOfTimesDepthMeasurementIncreases = array => {
+const countNumberOfTimesDepthMeasurementIncreases = (array) => {
   let count = 0;
   for (let i = 1; i < array.length; i++) {
     if (array[i] > array[i - 1]) {
@@ -18,14 +18,32 @@ const countNumberOfTimesDepthMeasurementIncreases = array => {
   return count;
 };
 
+const countNumberOfTimesDepthMeasurementIncreasesSlidingWindow = (array) => {
+  let count = 0;
+  let i = 0;
+  while (array[i + 3]) {
+    if (array[i + 1] + array[i + 2] + array[i + 3] > array[i] + array[i + 1] + array[i + 2]) {
+      count++;
+    }
+    i++;
+  }
+  return count;
+};
+
 const test = () => {
+  const testInput = prepareInput(testIputFilename);
   const count = countNumberOfTimesDepthMeasurementIncreases(testInput);
   assert.equal(count, 7);
+  const countSlidingWindow = countNumberOfTimesDepthMeasurementIncreasesSlidingWindow(testInput);
+  assert.equal(countSlidingWindow, 5);
 };
 
 const run = () => {
+  const input = prepareInput(inputFilename);
   const count = countNumberOfTimesDepthMeasurementIncreases(input);
-  console.log(`There are ${count} measurements that are larger than the previous measurement.`);
+  console.log(`A) There are ${count} measurements that are larger than the previous measurement.`);
+  const countSlidingWindow = countNumberOfTimesDepthMeasurementIncreasesSlidingWindow(input);
+  console.log(`B) When using sliding window, there are ${countSlidingWindow}.`);
 };
 
 test();
