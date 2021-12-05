36792d6339b8a31f89afb0a07159f6d0561b9100
François Hodierne <francois@hodierne.net>
2021-12-05 12:36:55 +0100

    day 4 part b
    ~30 minutes

--- day-4.js
+++ day-4.js
@@ -30,7 +30,13 @@ function checkGrid(grid) {
     if (line.length === line.filter((entry) => entry.marked === true).length) {
       return true;
     }
-    // TODO: implement column check
+  }
+  const columnCount = grid[0].length;
+  for (let column = 0; column < columnCount; column++) {
+    const columnEntries = grid.map((line) => line[column]);
+    if (columnEntries.length === columnEntries.filter((entry) => entry.marked === true).length) {
+      return true;
+    }
   }
 }
 
@@ -43,9 +49,9 @@ function sumOfAllUnmarkedNumbers(grid) {
   );
 }
 
-function game(numbers, grids) {
+function game(numbers, grids, type = 'returnWining') {
   for (let i = 1; i <= numbers.length; i++) {
-    console.log(`Round ${i}`);
+    // console.log(`Round ${i}`);
     const number = numbers[i - 1];
     for (const grid of grids) {
       for (const line of grid) {
@@ -58,10 +64,20 @@ function game(numbers, grids) {
       }
     }
 
-    for (const grid of grids) {
+    for (let i = 0; i < grids.length; i++) {
+      const grid = grids[i];
       const result = checkGrid(grid);
       if (result) {
-        return { number, grid };
+        if (type === 'returnWining') {
+          return { number, grid };
+        }
+        if (type === 'returnLosing') {
+          if (grids.length === 1) {
+            return { number, grid };
+          }
+          grids.splice(i, 1);
+          i--;
+        }
       }
     }
   }
@@ -69,20 +85,30 @@ function game(numbers, grids) {
 
 function test() {
   const [numbers, grids] = getInput(testIputFilename);
-  const { number, grid } = game(numbers, grids);
+  const { number, grid } = game(numbers, grids, 'returnWining');
   const sum = sumOfAllUnmarkedNumbers(grid);
   assert.equal(sum, 188);
   assert.equal(number, 24);
   assert.equal(sum * number, 4512);
+  const { number: numberLosing, grid: gridLosing } = game(numbers, grids, 'returnLosing');
+  const sumLosing = sumOfAllUnmarkedNumbers(gridLosing);
+  assert.equal(sumLosing, 148);
+  assert.equal(numberLosing, 13);
+  assert.equal(sumLosing * numberLosing, 1924);
 }
 
 function run() {
   const [numbers, grids] = getInput(inputFilename);
-  const { number, grid } = game(numbers, grids);
+  const { number, grid } = game(numbers, grids, 'returnWining');
   const sum = sumOfAllUnmarkedNumbers(grid);
-  console.log(`sum of all unmarked numbers is ${sum} `);
-  console.log(`the number that was just called ${number}`);
-  console.log(`multiplication is ${sum * number} `);
+  console.log(`A) sum of all unmarked numbers is ${sum} `);
+  console.log(` the number that was just called ${number}`);
+  console.log(` multiplication is ${sum * number} `);
+  const { number: numberLosing, grid: gridLosing } = game(numbers, grids, 'returnLosing');
+  const sumLosing = sumOfAllUnmarkedNumbers(gridLosing);
+  console.log(`B) sum of all unmarked numbers is ${sumLosing} `);
+  console.log(` the number that was just called ${numberLosing}`);
+  console.log(` multiplication is ${sumLosing * numberLosing} `);
 }
 
 test();
