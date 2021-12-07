2266d2880d50d327c25d405d0acad6a457e0ba9e
François Hodierne <francois@hodierne.net>
2021-12-07 21:33:41 +0100

    day 7 part b

--- day-7.js
+++ day-7.js
@@ -15,13 +15,26 @@ const getIndexedInput = (filename) => {
   }, Array(max + 1).fill(0));
 };
 
-function computeBestPosition(indexedInput) {
+function computeFuelForDistance(distance) {
+  let fuel = 0;
+  for (let i = 0; i < distance; i++) {
+    fuel += i + 1;
+  }
+  return fuel;
+}
+
+function computeBestPosition(indexedInput, version = 'a') {
   const positionFuels = {};
   for (let i = 0; i < indexedInput.length; i++) {
     let fuel = 0;
     for (let j = 0; j < indexedInput.length; j++) {
       const value = indexedInput[j];
-      fuel += Math.abs(i - j) * value;
+      const distance = Math.abs(i - j);
+      if (version === 'a') {
+        fuel += distance * value;
+      } else {
+        fuel += computeFuelForDistance(distance) * value;
+      }
     }
     positionFuels[i] = fuel;
     // console.log(`Fuel for position ${i} is ${fuel}`);
@@ -37,18 +50,35 @@ function computeBestPosition(indexedInput) {
 function test() {
   const indexedInput = getIndexedInput(testIputFilename);
 
-  const [minimumFuel, minimumPosition] = computeBestPosition(indexedInput);
+  const [minimumFuel, minimumPosition] = computeBestPosition(indexedInput, 'a');
 
   assert.equal(minimumFuel, 37);
   assert.equal(minimumPosition, 2);
+
+  assert.equal(computeFuelForDistance(11), 66);
+  assert.equal(computeFuelForDistance(5), 15);
+  assert.equal(computeFuelForDistance(3), 6);
+  assert.equal(computeFuelForDistance(1), 1);
+  assert.equal(computeFuelForDistance(2), 3);
+  assert.equal(computeFuelForDistance(4), 10);
+  assert.equal(computeFuelForDistance(9), 45);
+
+  const [minimumFuelB, minimumPositionB] = computeBestPosition(indexedInput, 'b');
+
+  assert.equal(minimumFuelB, 168);
+  assert.equal(minimumPositionB, 5);
 }
 
 function run() {
   const indexedInput = getIndexedInput(inputFilename);
 
-  const [minimumFuel, minimumPosition] = computeBestPosition(indexedInput);
+  const [minimumFuel, minimumPosition] = computeBestPosition(indexedInput, 'a');
 
-  console.log(`Minimum fuel is ${minimumFuel} at position ${minimumPosition}`);
+  console.log(`A) Minimum fuel is ${minimumFuel} at position ${minimumPosition}`);
+
+  const [minimumFuelB, minimumPositionB] = computeBestPosition(indexedInput, 'b');
+
+  console.log(`B) Minimum fuel is ${minimumFuelB} at position ${minimumPositionB}`);
 }
 
 test();
